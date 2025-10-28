a4bbe73f04f303ac2f18c9d7549d9c78192414b5
Bruno1776 <brunosantiago7002@gmail.com>
2025-10-28 20:30:49 -0300

    remove contact and about page

--- src/app/[locale]/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// 📁 app/[locale]/about/page.tsx
-
-'use client'
-
-import CompanyPage from '@/src/components/about/CompanyPage';
-
-export default function AboutPage() {
-  return <CompanyPage />;
-}
