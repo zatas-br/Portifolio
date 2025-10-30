82fefb379e67f54af7f9845d4f21db555e91cada
GabrielCardoso76 <torresgbriel8@gmail.com>
2025-10-29 22:36:42 -0300

    Add project data for Safety Training, EPI Stock, and Cash Flow platforms

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -41,10 +41,135 @@ export const PROJECTS_STATIC: ProjectStatic[] = [
     id: 'personal-app',
     category: 'desenvolvimento',
     image: '/images/projects/personal-app/hero.jpg',
-    technologies: ['React Native', 'Expo', 'Spring Boot', 'MySQL'],
+    technologies: ['React Native', 'Expo', 'Java', 'Spring Boot', 'MySQL'],
     link: null,
     gallery: [],
-    authors: []
+    authors: [
+       { 
+        name: 'Bruno Santiago', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Antony Brito', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Gabriel Cardoso', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      }
+    ]
+  },
+  {
+    id: 'gerenciador-de-alunos',
+    category: 'desenvolvimento',
+    image: '/images/projects/personal-app/hero.jpg',
+    technologies: ['Java', 'Gradlew', 'SQLite', 'JUnit', "Swing", "Flatlaf"],
+    link: null,
+    gallery:  [],
+    authors: [
+      { 
+        name: 'Bruno', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Jose', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      }
+    ]
+  },
+  {
+    id: 'trainingSec',
+    category: 'desenvolvimento',
+    image: '/images/projects/personal-app/hero.jpg',
+    technologies: ['Java', 'Gradlew', 'Spring', 'JUnit', "PostgreSQL", "H2 Database", "NextJS", "TypeScript", "Tailwind", "Radix"],
+    link: null,
+    gallery:  [],
+    authors: [
+      { 
+        name: 'Bruno', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Luigi', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      }
+    ]
+  },
+  {
+    id: 'EPI-stock',
+    category: 'desenvolvimento',
+    image: '/images/projects/personal-app/hero.jpg',
+    technologies: ['TypeScript', 'JavaScript', 'Node', 'MySQL', "Vite", "React", "Tailwind"],
+    link: null,
+    gallery:  [],
+    authors: [
+      { 
+        name: 'Bruno', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      }
+    ]
+  },
+  {
+    id: 'cash-flow',
+    category: 'desenvolvimento',
+    image: '/images/projects/personal-app/hero.jpg',
+    technologies: ['Java', 'H2DB', 'Spring', 'JUnit', "Thymeleaf", "expo GO", "React Native", "JavaScript"],
+    link: null,
+    gallery:  [],
+    authors: [
+      { 
+        name: 'Bruno', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Jose', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Willson', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Gustavo', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Camila', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+      { 
+        name: 'Roberto', 
+        avatar: '/images/team/bruno.jpg', 
+        role: 'Desenvolvedor Full-Stack',
+        linkedin: '...'
+      },
+    ]
   },
   // ... outros projetos
 ];
