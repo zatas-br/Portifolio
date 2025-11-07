0c3d4c897cdca7c97008872bc6a8ef31dbee7a49
Bruno1776 <brunosantiago7002@gmail.com>
2025-11-07 20:38:59 -0300

    refactor authors projects

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,37 +1,79 @@
-import { TeamProfile } from "@/types";
+import { ProjectAuthor } from "@/types";
 
-
-export const TEAM_PROFILES: TeamProfile[] = [
-  {
-    id: 'antony-brito',
-    image: 'https://i.postimg.cc/C1DKj84K/perfil-antony.jpg',
-    social: {
-      linkedin: 'https://linkedin.com/in/britoantony',
-      github: 'https://github.com/AntonyBrito'
-    }
+export const TEAM_AUTHORS: Record<string, ProjectAuthor> = {
+  "antony-brito": {
+    name: "Antony Brito",
+    avatar: "https://i.postimg.cc/C1DKj84K/perfil-antony.jpg",
+    role: "Head of Growth & Product",
+    linkedin: "https://www.linkedin.com/in/britoantony",
+    instagram: "https://www.instagram.com/antonybriito",
+    email: "brito@zatas.com.br",
+    github: "https://github.com/AntonyBrito"
   },
-  {
-    id: 'bruno-santiago',
-    image: 'https://i.postimg.cc/HnPJXpdv/runo.jpg',
-    social: {
-      linkedin: 'https://www.linkedin.com/in/bruno-santiago-b8169637b',
-      github: 'https://github.com/Bruno1776'
-    }
+  "bruno-santiago": {
+    name: "Bruno Santiago",
+    avatar: "https://i.postimg.cc/HnPJXpdv/runo.jpg",
+    role: "Head of Technology",
+    linkedin: "https://www.linkedin.com/in/bruno-santiago-b8169637b",
+    instagram: "https://www.instagram.com/bruno_s4nt",
+    email: "santiago@zatas.com.br",
+    github: "https://github.com/Bruno1776"
   },
-  {
-    id: 'gabriel-cardoso',
-    image: 'https://i.postimg.cc/1XYgw9sY/brya.jpg',
-    social: {
-      linkedin: 'https://www.linkedin.com/in/gabriel-cardoso-torres-b76a59328',
-      github: 'https://github.com/GabrielCardoso76'
-    }
+  "gabriel-cardoso": {
+    name: "Gabriel Cardoso",
+    avatar: "https://i.postimg.cc/1XYgw9sY/brya.jpg",
+    role: "Head of Finance & Operations",
+    linkedin: "https://www.linkedin.com/in/gabriel-cardoso-torres-b76a59328",
+    instagram: "https://www.instagram.com/gabrielcardos095",
+    email: "torres@zatas.com.br",
+    github: "https://github.com/GabrielCardoso76"
   },
-  {
-    id: 'thiago-bryan',
-    image: 'https://i.postimg.cc/fyg3mwD2/gabel.jpg',
-    social: {
-      linkedin: 'https://www.linkedin.com/in/thbryann',
-      twitter: 'https://www.instagram.com/thbryann'
-    }
+  "thiago-bryan": {
+    name: "Thiago Bryan",
+    avatar: "https://i.postimg.cc/fyg3mwD2/gabel.jpg",
+    role: "Head of marketing & design",
+    linkedin: "https://www.linkedin.com/in/thbryann",
+    instagram: "https://www.instagram.com/thbryann",
+    email: "bryan@zatas.com.br"
+  },
+  "jose": {
+    name: "Jose",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "luigi": {
+    name: "Luigi",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "willson": {
+    name: "Willson",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "gustavo": {
+    name: "Gustavo",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "camila": {
+    name: "Camila",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "roberto": {
+    name: "Roberto",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "raissa": {
+    name: "Raissa",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "juliana": {
+    name: "Juliana",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "joao-gabriel": {
+    name: "João Gabriel",
+    role: "Desenvolvedor Full-Stack"
+  },
+  "vitoria": {
+    name: "Vitoria",
+    role: "Desenvolvedor Full-Stack"
   }
-];
+};
